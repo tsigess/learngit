72bfdfc3cb4ac1b8fb54c829732af3f25373cac2
tsgu <samrawitshewarega@gmail.com>
2023-03-18 23:13:06 +0300

    working string

--- fun.dart
+++ fun.dart
@@ -8,4 +8,10 @@ void main() {
   printInfo(name: "Reecha", gender: "female");
   printInfo(name: "Harry", gender: "male");
   printInfo(gender: "Male", name: "santa");
+  printInfo(name: "alem", gender: "male");
+  printInfo(gender: "gember", name: "gad");
+}
+
+void printInf({String? name, String? gender}) {
+  print("Hello $name your gender is $gender.");
 }
